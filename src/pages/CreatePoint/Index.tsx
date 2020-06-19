7c826b79c60724c08177415959d12d6568118169
Leonardo Alvarenga <leo.alvarenga08@hotmail.com>
2020-06-19 13:24:30 -0300

    add comments

--- src/pages/CreatePoint/Index.tsx
+++ src/pages/CreatePoint/Index.tsx
@@ -91,8 +91,9 @@ const CreatePoint: React.FC = () => {
     }
 
     function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+        // name and value of the input
         const { name, value } = event.target;
-
+        // name of the input in []
         setFormData({ ...formData, [name]: value })
     }
 
